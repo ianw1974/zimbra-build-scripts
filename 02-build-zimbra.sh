2084bc6323f72e76e845f3f0f94d24b6e6c9d103
Ian Walker <ian@lsltd.org>
2020-10-15 17:00:52 +0200

    Minor fixes

--- 02-build-zimbra.sh
+++ 02-build-zimbra.sh
@@ -13,16 +13,20 @@ else
   mkdir $MAINDIR
 fi
 
-if [ -d "$PROJECTDIR" ]
+if [ -d "$MAINDIR/$PROJECTDIR" ]
 then
   echo "$PROJECTDIR directory exists, continuing..."
 else
   mkdir $MAINDIR/$PROJECTDIR
 fi
 
+cp config.build $MAINDIR/$PROJECTDIR
+cp zimbra-store.patch $MAINDIR/$PROJECTDIR
 cd $MAINDIR/$PROJECTDIR
 git clone https://github.com/zimbra/zm-build
+cd zm-build
 git checkout origin/develop
+cd ..
 cp config.build $MAINDIR/$PROJECTDIR/zm-build
 
 # Patch zimbra-store.sh to fix issue when convertd directory doesn't exist
