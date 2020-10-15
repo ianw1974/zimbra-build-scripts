8fdc6c04e84c025ee8f6da4a908180f2ee09c286
Ian Walker <ian.walker@neogage.com>
2020-10-15 11:56:47 +0200

    Initial script commit

---
+++ 01-install-build-deps.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#
+# Install build dependencies based on distro
+#
+# Supports:
+#     Ubuntu 18.04
+#     Ubuntu 16.04
+#     CentOS 8
+#     CentOS 7
+#
+# Required dependencies: lsb-release
+
+# Check if running as root
+if [ "$UID" == "0" ]
+then
+  echo "Running as root, all looking good."
+else
+  echo "Please run this script as root!."
+  exit 1
+fi
+
+# Get DISTRIB_ID or inform user of missing dependency
+if [ -f "/usr/bin/lsb_release" ]
+then
+  DISTRIB_ID=`lsb_release -i | awk '{print $3}'`
+else
+  echo "You need to install lsb-release or redhat-lsb package."
+  echo "Re-run script once installed."
+  exit 1
+fi
+
+# Start installing dependencies
+
+if [ $DISTRIB_ID == "Ubuntu" ]
+then
+  # Get release information
+  DISTRIB_RELEASE=`lsb_release -r | awk '{print $2}'`
+
+  # Check if running supported version and install dependencies
+  # or inform user of unsupported version and exit
+  if [ $DISTRIB_RELEASE == "16.04" ] || [ $DISTRIB_RELEASE == "18.04" ]
+  then
+    apt-get install -y software-properties-common openjdk-8-jdk ant ant-optional ant-contrib ruby git maven build-essential debhelper
+  else
+    echo "You are running an unsupported Ubuntu release!"
+    exit 1
+  fi
+elif [ $DISTRIB_ID == "CentOS" ]
+then
+  # Get release information
+  DISTRIB_RELEASE=`lsb_release -r | awk '{print $2}' | cut -f1 -d "."`
+
+  # Check if running supported version and install dependencies
+  # or inform user of unsupported version and exit
+  if [ $DISTRIB_RELEASE == "7" ]
+  then
+    yum groupinstall -y 'Development Tools'
+    yum install -y java-1.8.0-openjdk ant ant-junit ruby git maven cpan wget perl-IPC-Cmd rpm-build createrepo
+  elif [ $DISTRIB_RELEASE == "8" ]
+  then
+    dnf group install -y "Development Tools"
+    dnf module enable -y javapackages-tools
+    dnf install -y java-1.8.0-openjdk gcc-c++ ant-junit ruby git maven cpan wget rpm-build createrepo
+  else
+    echo "You are running an unsupported CentOS release!"
+    exit 1
+  fi
+else
+  echo "Unsupported distribution!"
+  echo "This script only supports CentOS 7/8 and Ubuntu 16.04/18.04"
+  exit 1
+fi
+
+# Dependencies completed
+echo "Dependencies installed.  Copy config.build to zm-build directory."
+echo "Edit config.build with appropriate parameters for your build!"
---
+++ 02-build-zimbra.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Script to build Zimbra
+
+# Variables
+MAINDIR=/git
+PROJECTDIR=zimbra
+BUILDDIR=zm-build
+BUILDNO="0001"
+BUILDTS=`date +'%Y%m%d%H%M%S'`
+BUILDREL="KEPLER"
+BUILDRELNO="9.0.0"
+
+if [ -d "$MAINDIR" ]
+then
+  echo "$MAINDIR directory exists, continuing..."
+else
+  mkdir $MAINDIR
+fi
+
+if [ -d "$PROJECTDIR" ]
+then
+  echo "$PROJECTDIR directory exists, continuing..."
+else
+  mkdir $MAINDIR/$PROJECTDIR
+fi
+
+cd $MAINDIR/$PROJECTDIR
+git clone https://github.com/zimbra/zm-build
+cp config.build $MAINDIR/$PROJECTDIR/zm-build/
+
+# Patch zimbra-store.sh to fix issue when convertd directory doesn't exist
+# else build will fail
+patch $MAINDIR/$PROJECTDIR/zm-build/instructions/bundling-scripts/zimbra-store.sh zimbra-store.patch
+
+# Change to build directory and build Zimbra
+cd $MAINDIR/$PROJECTDIR/$BUILDDIR
+./build.pl --build-no=$BUILDNO --build-ts=$BUILDTS --build-release=$BUILDREL--build-release-no=$BUILDRELNO --build-release-candidate=GA --build-type=FOSS --build-thirdparty-server=files.zimbra.com --no-interactive
+
+# Inform where archive can be found
+echo -e "\nZimbra archive file can be found under $MAINDIR/$PROJECTDIR/.staging\n"
