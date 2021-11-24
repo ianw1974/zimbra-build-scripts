4b445c7ab3200c98c4435699dee5891a757d4be4
Ian Walker <ian@lsltd.org>
2021-11-24 21:41:47 +0100

    Added support for building Ubuntu 20.04

--- zimbra-build-helper.sh
+++ zimbra-build-helper.sh
@@ -3,14 +3,14 @@
 ##################################
 # Zimbra Build Helper Script     #
 # Prepared By: Ian Walker        #
-# Version: 1.0.3                 #
+# Version: 1.0.4                 #
 #                                #
 # Supports:                      #
 #     CentOS 7/8                 #
 #     Oracle Linux 8             #
 #     RHEL Enterprise Server 7/8 #
 #     Rocky Linux 8              #
-#     Ubuntu 16.04/18.04         #
+#     Ubuntu 16.04/18.04/20.04   #
 ##################################
 
 #############
@@ -49,7 +49,7 @@ install_dependencies() {
     DISTRIB_RELEASE=`lsb_release -r | awk '{print $2}'`
 
     # Check if running supported version and install dependencies or inform user of unsupported version and exit
-    if [ ${DISTRIB_RELEASE} == "16.04" ] || [ ${DISTRIB_RELEASE} == "18.04" ]
+    if [ ${DISTRIB_RELEASE} == "16.04" ] || [ ${DISTRIB_RELEASE} == "18.04" ] || [ ${DISTRIB_RELEASE} == "20.04" ]
     then
       deb_pkg_install
     else
