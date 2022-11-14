ff57d4a89ff4278bf13ffdac2ac17de62c711001
Ian Walker <ian@lsltd.org>
2022-11-14 17:52:04 +0100

    Fix case for github

--- zimbra-build-helper.sh
+++ zimbra-build-helper.sh
@@ -199,7 +199,7 @@ help() {
   echo -e "${YELLOW}\nMAINDIR=${NORMAL}/home/git\n${YELLOW}PROJECTDIR=${NORMAL}zimbra\n"
   echo -e "${CYAN}Build summary step-by-step:\n${NORMAL}"
   echo -e "  ${YELLOW}1. Generate ssh key:${NORMAL} ssh-keygen -t rsa -b 4096 -C \"your_email@address\""
-  echo -e "  ${YELLOW}2. Upload this to your github profile:${NORMAL} https://github.com/settings/keys"
+  echo -e "  ${YELLOW}2. Upload this to your GitHub profile:${NORMAL} https://github.com/settings/keys"
   echo -e "  ${YELLOW}3. Only OpenJDK 8 can be installed on the build server, remove other versions${NORMAL}"
   echo -e "  ${YELLOW}4. Run:${NORMAL}./zimbra-build-helper.sh --install-deps"
   echo -e "  ${YELLOW}5. Run:${NORMAL}./zimbra-build-helper.sh --build-zimbra\n"
